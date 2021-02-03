a0e9463cff391e23f751c5a2a97e1b08d610e274
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2021-02-03 20:16:33 -0300

    JLF

--- Class-08-AdvancedExpress/src/products.ts
+++ Class-08-AdvancedExpress/src/products.ts
@@ -10,6 +10,16 @@ export const products = (app: Application) => {
 
   let products: Array<Product> = []
 
+  const get = (id: string) => {
+    const product = products.find(p => p.id === Number(id))
+
+    if (product) {
+      return product
+    } else {
+      return null
+    }
+  }
+
   app.get('/products', (req: Request, res: Response) => {
     if (!products.length) res.status(404).send({ error: "there're not products available" })
 
@@ -18,25 +28,58 @@ export const products = (app: Application) => {
 
   app.get('/products/:id', (req: Request, res: Response) => {
     const id = req.params.id
-    const product = products.find(p => p.id === Number(id))
 
-    if (!product) res.status(404).send({ error: 'product not found' })
-    res.status(201).send(product)
+    const product = get(id)
+    if (product) {
+      res.status(200).send(product)
+    } else {
+      res.status(404).send({ error: 'product not found' })
+    }
   })
 
   app.post('/products', (req: Request, res: Response) => {
-    let product: Product = { id: products.length + 1, ...req.body }
+    const product: Product = { id: products.length + 1, ...req.body }
     products = [...products, product]
 
-    res.status(200).send(product)
+    res.status(201).send(product)
   })
 
-  // app.patch('/products', (req: Request, res: Response) => {
-  //   let product: Product = { id: products.length + 1, ...req.body }
-  //   products = [...products, product]
+  app.put('/products', (req: Request, res: Response) => {
+    const updated: Product = { ...req.body }
 
-  //   res.status(200).send(product)
-  // })
+    const productIndex = products.findIndex(p => p.id === updated.id)
+    if (~productIndex) {
+      products[productIndex] = updated
+      res.status(200).send()
+    } else {
+      res.status(404).send({ error: 'product not found' })
+    }
+  })
+
+  app.patch('/products/:id/price', (req: Request, res: Response) => {
+    const id = req.params.id
+    const { price } = req.body
+
+    const product = get(id)
+    if (product) {
+      product.price = price
+      res.status(200).send()
+    } else {
+      res.status(404).send({ error: 'product not found' })
+    }
+  })
+
+  app.delete('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const product = get(id)
+    if (product) {
+      products = products.filter(p => p.id !== Number(id))
+      res.status(200).send()
+    } else {
+      res.status(404).send({ error: 'product not found' })
+    }
+  })
 }
 
 export default products
