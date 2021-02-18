e69ee7e66bbebca2059f728412ea5bd318234514
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2021-02-17 21:59:24 -0300

    JLF

--- Class-10-PlantillaMotores-I-hbs/src/index.ts
+++ Class-10-PlantillaMotores-I-hbs/src/index.ts
@@ -1,8 +1,4 @@
-import express, { Application, Request, Response } from 'express'
-import productsRouter, { products } from './products'
-//import productsViewRouter from './products.view'
-import handlebars from 'express-handlebars'
-import console from 'console'
+import express, { Application } from 'express'
 
 const PORT = 8080
 const app: Application = express()
@@ -12,25 +8,4 @@ app.use(express.urlencoded({ extended: true }))
 
 app.listen(PORT, () => console.log(`⚡️ http://localhost:${PORT}`))
 
-//Routes
-app.use('/products/api', productsRouter)
-//app.use('/products/view', productsViewRouter)
-
-//Template
-app.engine(
-  'hbs',
-  handlebars({
-    extname: '.hbs',
-    defaultLayout: 'index.hbs',
-    layoutsDir: './views/layouts/',
-    partialsDir: './views/partials/',
-  })
-)
-
-app.set('views', './views')
-app.set('view engine', 'hbs')
-app.use(express.static('public'))
-
-app.get('/products/view', (req: Request, res: Response) => {
-  res.render('main', { products: products, hasProducts: products.length !== 0 })
-})
+import('./routes').then(r => r.default(app))
---
+++ Class-10-PlantillaMotores-I-hbs/src/routes/products.view.ts
@@ -0,0 +1,52 @@
+import express, { Application, Request, Response } from 'express'
+
+import handlebars from 'express-handlebars'
+import { products } from './products'
+
+const productsView = (app: Application) => {
+  //setup
+  app.engine(
+    'hbs',
+    handlebars({
+      extname: '.hbs',
+      defaultLayout: 'index.hbs',
+      layoutsDir: './views/layouts/',
+      partialsDir: './views/partials/',
+    })
+  )
+
+  app.set('views', './views')
+  app.set('view engine', 'hbs')
+  app.use(express.static('public'))
+
+  //apis
+  app.get('/products/view', (req: Request, res: Response) => {
+    res.render('main', { products: products, hasProducts: products.length !== 0 })
+  })
+}
+
+// const routing = (app: Application) => {
+//   //setup
+//   app.engine(
+//     'hbs',
+//     handlebars({
+//       extname: '.hbs',
+//       defaultLayout: 'index.hbs',
+//       layoutsDir: './views/layouts/',
+//       partialsDir: './views/partials/',
+//     })
+//   )
+
+//   app.set('views', './views')
+//   app.set('view engine', 'hbs')
+//   app.use(express.static('public'))
+
+//   //apis
+//   app.get('/products/view', (req: Request, res: Response) => {
+//     res.render('main', { products: products, hasProducts: products.length !== 0 })
+//   })
+// }
+
+// export default routing
+
+export default productsView
