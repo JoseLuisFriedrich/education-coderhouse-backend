72621867239c9b0383bd8fe98da3fb682f2af71e
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2021-04-01 00:54:39 -0300

    JLF

--- Class-20-MongoDB-Node/src/database/mongoDb.ts
+++ Class-20-MongoDB-Node/src/database/mongoDb.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const connect = (): any => {
-  const connString = process.env.DB_CONNECTION || ''
+  const connString = process.env.DB_CONNECTION || 'mongodb://127.0.0.1/jlf_coderhouse_ecommerce'
   mongoose.connect(connString, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => {
     console.error(err)
   })
