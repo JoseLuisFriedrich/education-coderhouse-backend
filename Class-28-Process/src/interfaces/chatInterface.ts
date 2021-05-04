6f14d64fa72f974b7340146365c16aa9f6002350
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2021-05-03 23:03:56 -0300

    JLF

--- Class-28-Process/src/interfaces/chatInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose'
-import { IUser } from './userInterface'
-
-// interface
-export interface IMessage extends Document {
-  date: string
-  text: string
-  user: IUser
-}
--- Class-28-Process/src/interfaces/productInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose'
-
-// interface
-export interface IProduct extends Document {
-  id: number
-  title: string
-  price: number
-  thumbnail: string
-}
