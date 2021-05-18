d48c26e0409881f97bbe47c759ff27d097f64789
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2021-05-18 09:25:50 -0300

    JLF

---
+++ Class-32-Profiling/src/index.ts
@@ -0,0 +1,86 @@
+import express, { Application } from 'express'
+import session from 'express-session'
+import dbStore from 'connect-mongo'
+import dotenv from 'dotenv'
+import cluster from 'cluster'
+import cookieParser from 'cookie-parser'
+import facebookAuth from './auth/facebookAuth'
+import compression from 'compression'
+import logger from './helpers/logHelper'
+
+import * as os from 'os'
+import * as msc from './helpers/miscHelper'
+import * as db from './database/mongoDb'
+import * as socketio from 'socket.io'
+import * as httpLib from 'http'
+import * as path from 'path'
+
+import routes from './routes'
+
+const PORT = msc.arg(0, process.env.APP_PORT || 8080)
+const CLUSTER = msc.arg(1, 'Single') === 'Cluster'
+
+if (cluster.isMaster && CLUSTER) {
+  const cpuCount = os.cpus().length
+  logger.log('info', `⚡️ http://localhost:${PORT} - 💻 pid: ${process.pid} (Master x${cpuCount} cpus)`)
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', () => {
+    logger.log('info', `⚡️ http://localhost:${PORT} - 💻 pid: ${process.pid} stopped`)
+    cluster.fork()
+  })
+} else {
+  // config
+  dotenv.config()
+
+  // app config
+  const app: Application = express()
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.static(path.join(__dirname, './Views')))
+  app.use(cookieParser())
+  app.use(compression())
+  app.use(
+    session({
+      store: dbStore.create({ mongoUrl: db.connString(), ttl: 600 }),
+      secret: 'JLF',
+      resave: false,
+      saveUninitialized: false,
+      rolling: true,
+      cookie: {
+        httpOnly: false,
+        secure: false,
+        maxAge: 6000 * 1000,
+      },
+    })
+  )
+
+  // main
+  const http = new httpLib.Server(app)
+  const io = new socketio.Server(http)
+  http.listen(PORT, () => logger.log('info', `⚡️ http://localhost:${PORT} - 💻 pid: ${process.pid}`))
+
+  // auth
+  facebookAuth(app)
+
+  // database
+  db.connect()
+
+  // routing
+  routes(app, io)
+
+  // cleanup
+  const exitHandler = () => {
+    // db
+    db.close()
+
+    // exit
+    process.exit()
+  }
+
+  // shut down events
+  const exitTypes = ['SIGINT', 'SIGUSR1', 'SIGUSR2', 'SIGTERM', 'uncaughtException', 'exit']
+  exitTypes.forEach(eventType => process.on(eventType, exitHandler))
+}
---
+++ Class-32-Profiling/src/models/userModel.ts
@@ -0,0 +1,39 @@
+import { model, Model, Schema } from 'mongoose'
+
+import logger from '../helpers/logHelper'
+import { IUser } from '../interfaces/userInterface'
+
+// schema
+const UserSchema: Schema = new Schema({
+  id: { type: String, required: true },
+  userName: { type: String, required: true },
+  pictureUrl: { type: String, required: false },
+  isAdmin: { type: Boolean, required: true },
+})
+
+// class
+export const User: Model<IUser> = model('User', UserSchema)
+
+export const userGetById = async id => {
+  return await User.findOne({ id: id })
+}
+
+export const userInsert = async (user: IUser) => {
+  try {
+    await new User(user).save()
+  } catch (ex) {
+    logger.log('error', ex)
+  }
+}
+
+export const userUpdateIsAdmin = async (userName, isAdmin) => {
+  let result: number | undefined = 0
+
+  try {
+    result = (await User.updateOne({ userName: userName }, { isAdmin: isAdmin })).nModified
+  } catch (ex) {
+    logger.log('error', ex)
+  }
+
+  return result === 1
+}
